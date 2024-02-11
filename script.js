20459af47f2a7d8bd8219059773fd445092c2def
Papulin01 <159704523+Papulin01@users.noreply.github.com>
2024-02-11 12:01:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "AQUI EN MI CORAZON TU MANDA Y TU PONES LAS CONDICIONES CHEKETETA";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -42,12 +42,12 @@ function resizeYesButton() {
 
 function generateMessage(noCount) {
   const messages = [
-    "No",
-    "Are you sure?",
-    "Pookie please",
-    "Don't do this to me :(",
-    "You're breaking my heart",
-    "I'm gonna cry...",
+    "NO",
+    "SEGURA?",
+    "SEGURISIMA",
+    "A POCO SI MUY VERGA",
+    "ORALE PUES...",
+    "DUREN",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
